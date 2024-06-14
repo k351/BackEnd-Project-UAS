0962c342d5aab1b52e9147a659a3dd363382f609
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-06-15 01:00:36 +0700

    Middleware usage in app.php

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::user()->usertype !='admin'){
+        if(Auth::user()->type !='admin'){
             return redirect('/');
         }
 
