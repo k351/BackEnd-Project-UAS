173f88ad51b009ec9585edf9a7307d26cd9c7ece
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-06-15 00:11:47 +0700

    Added admin dashboard

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="d-flex align-items-stretch">
+  <!-- Sidebar Navigation-->
+  <nav id="sidebar">
+    <!-- Sidebar Header-->
+    <div class="sidebar-header d-flex align-items-center">
+      <div class="avatar"><img src="{{asset('/admincss/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
+      <div class="title">
+        <h1 class="h5">Christopher Wijaya</h1>
+        <p>Web Designer</p>
+      </div>
+    </div>
+    <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+    <ul class="list-unstyled">
+            <li class="active"><a href="/admin/dashboard"> <i class="icon-home"></i>Home </a></li>
+
+            <li>
+              <a href="{{url('view_category')}}"> <i class="icon-grid"></i>Category 
+              </a>
+          </li>
+    </ul>
+  </nav>
+  <!-- Sidebar Navigation end-->
