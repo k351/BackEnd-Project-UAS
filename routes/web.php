8603898c238219d31a4a36bd45f3c60048b5e8bb
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-06-14 23:57:55 +0700

    Added home view

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
