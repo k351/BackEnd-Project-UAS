16b74fb2b21fe0f538030746f1737f0f362e1282
briggea <geasandhea19@gmail.com>
2024-06-14 13:30:45 +0700

    modified users migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->unique();
+            $table->id();
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('type');
+            $table->enum('type', ['client', 'seller', 'admin'])->default('client');
             $table->string('address');
-            $table->integer('wallet_balance');
+            $table->integer('wallet_balance')->default(0);
         });
     }
 
