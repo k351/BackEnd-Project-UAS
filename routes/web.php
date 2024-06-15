5fe408a8de1c5dd06f90e472f885626aa360e3c7
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-06-15 22:51:03 +0700

    Base method to get checkout page and post the data

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/transaction', [ProfileController::class]); # this
+    Route::post('/transaction', [ProfileController::class]); # this
 });
 
 require __DIR__.'/auth.php';
