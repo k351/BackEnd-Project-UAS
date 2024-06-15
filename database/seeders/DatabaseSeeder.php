0eac2f976353f992e9baa5a2e2c93812914ecb4f
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-06-15 23:12:32 +0700

    Add timestamps column in Categories table
    Modify category models, call category seeder in
    DatabaseSeeder, add column in migrations.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(AdminSeeder::class);
+        $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class
+        ]);
     }
 }
