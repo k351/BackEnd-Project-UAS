35a702bf9419c9320a387a4dd01bf54a5ca9fa85
erycaaaaa <eryca847@gmail.com>
2024-06-17 00:55:02 +0700

    add feature update in WishlistController

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,21 +2,29 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-
 use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
-    //user 1
-    public function update_Wishlist($product_id)
+public function update_Wishlist($product_id)
     {
-        // insert wishlist
-        DB::table('wishlist')->insert([
-            'product_id' => $product_id,
-            'customer_id' => 1,
-        ]);
-        //delete wishlist (wip)
-        return redirect()->route('home');
+    // Asumsikan customer id = 1
+        $customer_id = 1;
 
+    // Cek produk ada di wishlist customer atau belum
+    $exists = DB::table('wishlist')->where('product_id', $product_id)
+                ->where('customer_id', $customer_id)->exists();
+
+    if ($exists) {
+     // kalau product belum ada, bakal ditambahkan
+            DB::table('wishlist')->insert(['product_id' => $product_id, 'customer_id' => $customer_id,]);
+    } else {
+     // kalo ada , wishlist akan terhapus
+            DB::table('wishlist')->where('product_id', $product_id)
+            ->where('customer_id', $customer_id)->delete();
+    }
+
+   //redirect ke home setelah mengupdate wishlist
+    return redirect()->route('home');
     }
 }
