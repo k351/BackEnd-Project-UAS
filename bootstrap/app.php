772c247d3ba08b33f20d7c3aa4e4c9e1c24483a6
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-06-16 22:38:57 +0700

    Change preventbackhistory class into prevent and added databaseseeder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\Admin::class,
+            'prevent' => \App\Http\Middleware\PreventBackHistory::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
