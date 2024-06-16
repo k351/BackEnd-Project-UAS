ddbfb1e05ca2d99254445827cc20fa1c5e8543a0
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-06-16 23:14:23 +0700

    Added seller and shop functionality

---
+++ app/Models/Shop.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'shop_name',
+        'seller_id',
+    ];
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class, 'seller_id');
+    }
+}
---
+++ resources/views/seller/register.blade.php
@@ -0,0 +1,19 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register.seller') }}">
+        @csrf
+
+        <!-- Shop Name -->
+        <div>
+            <x-input-label for="name" :value="__('Shop Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+
+            <x-primary-button class="ms-4">
+                {{ __('Register as Seller') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
