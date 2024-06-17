0b72acfaa517317f40308b55a7056bbc95a08a9b
briggea <geasandhea19@gmail.com>
2024-06-17 12:00:38 +0700

    Fixed duplicate lines

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -6,32 +6,11 @@
             </h2>
         </div>
         <div class="row">
-
-            @foreach($product as $product)
-            <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="box">
-                <div class="img-box">
-                    <img src="images/{{$product->image}}" alt="">
-                </div>
-                <div class="detail-box">
-                    <h6>{{$product->name}}</h6>
-                    <h6>Price
-                    <span>{{$product->price}}</span>
-                    </h6>
-                </div>
-
-                <div style="padding:15px">
-                    <a class="btn btn-danger" href="{{url('product_details', $product->id)}}">Details</a>
-                </div>
-            </div>
-            </div>
-            <div class="row">
-
                 @foreach($product as $product)
                 <div class="col-sm-6 col-md-4 col-lg-3">
                 <div class="box">
                     <div class="img-box">
-                        <img src="products/{{$product->image}}" alt="">
+                        <img src="images/{{$product->image}}" alt="">
                     </div>
                     <div class="detail-box">
                         <h6>{{$product->name}}</h6>
@@ -62,6 +41,4 @@
                 </div>
 
                 @endforeach
-            </div>
-            </div>
         </section>
