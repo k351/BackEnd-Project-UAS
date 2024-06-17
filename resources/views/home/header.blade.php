346fa3459cfd61ec7a390f721441f40b3dd5a952
briggea <geasandhea19@gmail.com>
2024-06-17 12:03:07 +0700

    Fixed shop button destination

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -15,7 +15,7 @@
           <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="shop.html">
+          <a class="nav-link" href="{{url('shop_page')}}">
             Shop
           </a>
         </li>
