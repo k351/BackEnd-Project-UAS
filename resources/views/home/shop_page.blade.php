0523b46e0e11087134dfa7a4804dfbd915d7ba2f
briggea <geasandhea19@gmail.com>
2024-06-17 12:04:23 +0700

    Added new page to view more products in shop page

---
+++ resources/views/home/shop_page.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    @include('home.header')
+    <!-- end header section -->
+   
+    <!-- end hero area -->
+    @include('home.product')
+    <!-- shop section -->
+
+  <!-- info section -->
+    @include('home.footer')
+</body>
+
+</html>
