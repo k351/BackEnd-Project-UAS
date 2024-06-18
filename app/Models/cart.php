92e2b5f68da25064cc83796b040b47a6e14e8283
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-06-18 11:24:06 +0700

    Fix cart models name and migration cart table name

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class carts extends Model
+class Cart extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'product_id', 'quantity'];
