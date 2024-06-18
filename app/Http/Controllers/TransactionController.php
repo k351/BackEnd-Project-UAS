ced91fab0ee137f7185fd45bb06d306628e8a045
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-06-18 11:20:44 +0700

    Add create transaction method Create transaction if user balance is greater than total price. Delete cart because already processed. Decrease user balance and add the balance to seller.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
     public function index($id) {
-        $data = Product::find($id);
+        $product = Product::find($id);
 
         return view("transaction.index", [
-            'product' => $data,
+            'product' => $product,
         ]);
     }
 
@@ -54,5 +56,44 @@ class TransactionController extends Controller
             'product' => $product,
             'cart' => $cart,
         ]);
+    }
+
+    public function create(Request $request, $id)
+    {
+        $user = Auth::user();
+        $cart = Cart::where('user_id', $user->id)
+                    ->where('product_id', $id)
+                    ->firstOrFail();
+
+        $product = Product::findOrFail($id);
+        $totalAmount = ($product->price * $cart->quantity) + 3000; // Calculate the total amount
+
+        if ($user->wallet_balance < $totalAmount) {
+            return redirect()->route('transaction.checkout', ['id' => $id])
+                ->with('error', 'Saldo tidak mencukupi untuk melakukan transaksi ini.');
+        }
+
+        $user->wallet_balance -= $totalAmount;
+        $user->save();
+
+        $seller = $product->shop->seller;
+
+        if ($seller) {
+            // Calculate seller's earning (example: minus service fees)
+            $sellerEarning = $totalAmount - 3000; // Assuming fees are deducted from the total amount
+
+            // Update seller's wallet balance
+            $seller->wallet_balance += $sellerEarning;
+            $seller->save();
+        }
+
+        $transaction = Transaction::create([
+            'customer_id' => $user->id,
+            'total' => $totalAmount,
+        ]);
+
+        $cart->delete();
+
+        return redirect()->route('home');
     }
 }
