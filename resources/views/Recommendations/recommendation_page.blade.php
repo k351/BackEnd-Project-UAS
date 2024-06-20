60b976d89e37c90434394856537e9bae76a34b7f
Brenda Abigail <brendaabgl@gmail.com>
2024-06-20 15:25:07 +0700

    menambahkan recommendation page

---
+++ resources/views/Recommendations/recommendation_page.blade.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    @include('home.header')
+    <!-- end header section -->
+  </div>
+  <!-- end hero area -->
+    @include('Recommendations.recommendation')
+  <!-- shop section -->
+
+  <!-- end shop section -->
+
+  <!-- contact section -->
+    @include('home.contact')
+
+  <!-- end contact section -->
+
+  <!-- info section -->
+    @include('home.footer')
+</body>
+
+</html>
