83eae8a1952be00c9e11564c67ed28d8299644fd
Brenda Abigail <brendaabgl@gmail.com>
2024-06-20 10:55:57 +0700

    update rekomendasi controller

--- app/Http/Controllers/RekomendasiController.php
+++ app/Http/Controllers/RekomendasiController.php
@@ -8,17 +8,20 @@ use Illuminate\Support\Facades\DB;
 
 class RekomendasiController extends Controller
 {
-    //
-
     public function index()
-    {
-        // Mengambil produk yang direkomendasikan (contoh sederhana)
+{
+    try {
         $recommendedProducts = Product::inRandomOrder()->limit(10)->get();
+        if ($recommendedProducts->isEmpty()) {
+            return view('recomendations.recomendation', ['message' => 'No recommended products found.']);
+        }
         $wishlist = DB::table('wishlist')
             ->select('id', 'product_id')
             ->where('customer_id', 1)
             ->get()->toArray();
-
         return view('recomendations.recomendation', compact('recommendedProducts', 'wishlist'));
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
     }
+}
 }
