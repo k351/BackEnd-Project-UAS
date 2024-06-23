d463bbfdf00ff2b68a60d1d1600bc777042e5a34
erycaaaaa <eryca847@gmail.com>
2024-06-23 19:19:17 +0700

    Modified status column to allow null values

--- database/migrations/2024_06_17_133020_create_carts_table.php
+++ database/migrations/2024_06_17_133020_create_carts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId("product_id")->constrained("products");
             $table->foreignId("user_id")->constrained("users");
             $table->bigInteger("quantity");
+            $table->integer("status")->nullable();
             $table->timestamps();
         });
     }
