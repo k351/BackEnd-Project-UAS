14ec70d80f0adcc05e0253dc425d9bec7a85ae1b
erycaaaaa <eryca847@gmail.com>
2024-06-23 19:24:15 +0700

    Added 'status' attribute

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'product_id', 'quantity'];
+    protected $fillable = ['user_id', 'product_id', 'quantity','status'];
 
     public function product()
     {
