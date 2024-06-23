6f6162c05dc026caec93b0746b0bf4ea972a37fc
erycaaaaa <eryca847@gmail.com>
2024-06-23 19:34:12 +0700

    add function in cart view

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -12,62 +12,86 @@
 <body>
     @include('home.header')
 
-        <div class="container my-4">
-            <h1>Keranjang</h1>
+    <div class="container my-4">
+        <h1>Keranjang</h1>
 
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-            <form action="{{ route('cart.update') }}" method="POST">
-                @method("PUT")
-                @csrf
-                <table class="table">
-                    <thead>
+        <form action="{{ route('cart.update') }}" method="POST">
+            @method('PUT')
+            @csrf
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Produk</th>
+                        <th>Harga</th>
+                        <th>Jumlah</th>
+                        <th>Subtotal</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($cartItems as $item)
                         <tr>
-                            <th></th>
-                            <th>Produk</th>
-                            <th>Harga</th>
-                            <th>Jumlah</th>
-                            <th>Subtotal</th>
-                            <th></th>
+                            <td>
+                                <input type="checkbox" name="selectedItems[]"
+                                    value="{{ $item->product->id }}"id="item_{{ $item->id }}">
+                            </td>
+                            <td>
+                                <img src="{{ asset('products/' . $item->product->image) }}"
+                                    alt="{{ $item->product->name }}" style="width: 50px;">
+                                {{ $item->product->name }}
+                            </td>
+                            <td>Rp {{ number_format($item->product->price, 0, '', '.') }}</td>
+                            <td>
+                                <input type="number" name="cart[{{ $item->product->id }}]"
+                                    value="{{ $item->quantity }}" min="1" style="width: 50px;">
+                            </td>
+                            <td>Rp {{ number_format($item->product->price * $item->quantity, 0, '', '.') }}</td>
+                            <td>
+                                <a href="{{ route('cart.delete_cart_item', $item->id) }}"
+                                    class="btn btn-danger btn-sm">Hapus</a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($cartItems as $item)
-                            <tr>
-                                <td>
-                                    <input type="checkbox" name="selectedItems[]" value="{{ $item->product->id }}">
-                                </td>
-                                <td>
-                                    <img src="{{ asset('products/' . $item->product->image) }}"
-                                        alt="{{ $item->product->name }}" style="width: 50px;">
-                                    {{ $item->product->name }}
-                                </td>
-                                <td>Rp {{ number_format($item->product->price, 0, '', '.') }}</td>
-                                <td>
-                                    <input type="number" name="cart[{{ $item->product->id }}]"
-                                        value="{{ $item->quantity }}" min="1" style="width: 50px;">
-                                </td>
-                                <td>Rp {{ number_format($item->product->price * $item->quantity, 0, '', '.') }}</td>
-                                <td>
-                                    <a href="{{ route('cart.delete', $item->id) }}"
-                                        class="btn btn-danger btn-sm">Hapus</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <button type="submit" class="btn btn-primary my-2">Update Keranjang</button>
-            </form>
+                    @endforeach
+                </tbody>
+            </table>
+            <button type="submit" class="my-2 btn btn-primary">Update Keranjang</button>
+        </form>
 
-            <form action="{{ route('cart.checkout') }}" method="POST">
-                @csrf
-                <button type="submit" class="btn btn-success">Beli</button>
-            </form>
-        </div>
+        <form id="checkoutForm" action="{{ route('cart.checkout') }}" method="POST">
+            @csrf
+            <!-- Hidden input field to store selected items -->
+            <input type="hidden" id="selectedItemsInput" name="selectedItems">
+
+            <button type="button" onclick="prepareCheckout()" class="btn btn-success">Beli</button>
+        </form>
+
+
+
+        <script>
+            function prepareCheckout() {
+                // Find all checked checkboxes in the table
+                var selectedItems = [];
+                var checkboxes = document.querySelectorAll('input[name="selectedItems[]"]:checked');
+
+                checkboxes.forEach(function(checkbox) {
+                    selectedItems.push(checkbox.value);
+                });
+
+                // Set the value of hidden input field to selected items
+                document.getElementById('selectedItemsInput').value = JSON.stringify(selectedItems);
+
+                // Submit the form
+                document.getElementById('checkoutForm').submit();
+            }
+        </script>
+    </div>
 
     @include('home.footer')
 </body>
